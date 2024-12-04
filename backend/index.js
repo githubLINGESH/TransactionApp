0481ef15a09889e76a62c479f975342e429c5fa3
SharaanV <sharaan225@gmail.com>
2024-12-04 21:42:13 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,11 @@ const bodyparser = require('body-parser')
 const router = require('./routes/index')
 const cors = require('cors')
 const app = express()
-
+app.use(cors({
+    origin: 'https://dealon.onrender.com',
+    credentials: true
+}));
+app.set('trust proxy', 1);
 app.use(cors({origin:"https://transactionapp-m0m6.onrender.com/"}))
 app.use(bodyparser.json())
 app.use('/api/v1',router)
