ba9b02b6d2dfa3d2db0bf15112729e9c56e54c3d
githubLINGESH <107837353+githubLINGESH@users.noreply.github.com>
2024-12-04 15:05:58 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,8 @@ const cors = require('cors')
 const app = express()
 
 app.use(cors({origin:"https://transactionapp-m0m6.onrender.com/"}))
+// Set up Express sessions
+app.set('trust proxy', 1); // Trust the first proxy
 app.use(bodyparser.json())
 app.use('/api/v1',router)
 
